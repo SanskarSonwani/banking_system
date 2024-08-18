7e4225b4ac055fbfe5853ed867626ad6da594600
w3schools-test <sanskarsonwani007@gmail.com>
2024-08-18 12:17:16 +0530

    Implement Home UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,29 @@
+import { Section } from 'lucide-react'
 import React from 'react'
+import HeaderBox from '@/components/ui/headerbox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 
 const Home = () => {
+  const loggedIn = {firstName : 'Adrian'};
   return (
-    <div>
-      Home
+   <section className="home">
+    <div className="home-content">
+      <header className='home-header'>
+       <HeaderBox
+       type="greeting"
+       title="Welcome"
+       user={loggedIn?.firstName||'Guest'}
+       subtext="Acces and manage your account and transactonns efficiently."
+       />
+      </header>
+      <TotalBalanceBox 
+      accounts = {[]}
+      totalBanks={1}
+      totalCurrentBalance={1250.35}
+      />
+
     </div>
+   </section>
   )
 }
 
