f1320749f839b5d1cb69f57ad2810389572281bf
w3schools-test <sanskarsonwani007@gmail.com>
2024-09-22 19:41:46 +0530

    Implmented sidebars

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,29 @@
+import Sidebar from '@/components/ui/Sidebar';
+import MobileNav from '@/components/ui/MobileNav';
+import Image from 'next/image';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName :'Adrain', lastName : 'JSM'};
   return (
-  <main>
-SIDEBAR
-{children}
+  <main className='flex h-screen w-full font-inter'>
+ <Sidebar user={loggedIn} />
+
+   <div className="flex size-full flex-col ">
+    <div className="root-layout">
+      <Image src="/icons/logo.svg" width={30} height={30} alt='menu icon' />
+<div>
+   <MobileNav user={loggedIn} />
+</div>
+
+    </div>
+    {children}
+  </div> 
+  
+
   </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,10 @@ import { Section } from 'lucide-react'
 import React from 'react'
 import HeaderBox from '@/components/ui/headerbox'
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
+import RightSidebar from '@/components/ui/RightSidebar'
 
 const Home = () => {
-  const loggedIn = {firstName : 'Adrian'};
+  const loggedIn = {firstName : 'Adrian', lastName:'JSM', email:'AdJSM@gmail.com'};
   return (
    <section className="home">
     <div className="home-content">
@@ -15,14 +16,19 @@ const Home = () => {
        user={loggedIn?.firstName||'Guest'}
        subtext="Acces and manage your account and transactonns efficiently."
        />
-      </header>
-      <TotalBalanceBox 
+        <TotalBalanceBox 
       accounts = {[]}
       totalBanks={1}
       totalCurrentBalance={1250.35}
       />
+      </header>
+     Recent Transactions
 
     </div>
+    <RightSidebar user={loggedIn}
+    transactions={[]}
+    banks = {[{currentBalance: 123.50},{currentBalance: 150.50}]}
+    />
    </section>
   )
 }
